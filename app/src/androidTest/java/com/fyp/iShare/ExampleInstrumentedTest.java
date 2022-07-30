e106682e9a8418a0cf3bc113ab8a707803e6bb38
Hudaifa Abdullah ツ <loffy@live.com>
2022-07-31 00:37:50 +0500

    more renaming

--- app/src/androidTest/java/com/fyp/iShare/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/fyp/iShare/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.fyp.awacam", appContext.getPackageName());
+        assertEquals("com.fyp.iShare", appContext.getPackageName());
     }
 }
