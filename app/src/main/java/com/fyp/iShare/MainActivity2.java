9fe4dc73ca26df07cd2ccad01079e488e8f42830
aqibn <aqibnaveed272@gmail.com>
2022-08-17 19:44:08 +0500

    Revert "Merge remote-tracking branch 'origin/master' into master"
    This reverts commit 8e2e603e575291ef63ed547ca4c01b8d6f32f2dc, reversing
    changes made to 44a28d3e530b4c64838ee3ed3050afef19aaa086.

--- app/src/main/java/com/fyp/iShare/MainActivity2.java
+++ app/src/main/java/com/fyp/iShare/MainActivity2.java
@@ -6,15 +6,12 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.preference.PreferenceManager;
 
 import com.fyp.iShare.databinding.ActivityMain2Binding;
-import com.fyp.iShare.ui.devices.DevicesFragment;
-import com.fyp.iShare.ui.downloads.DownloadsFragment;
-import com.fyp.iShare.ui.home.HomeFragment;
-import com.fyp.iShare.ui.messages.MessagesFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -30,53 +27,36 @@ public class MainActivity2 extends AppCompatActivity {
 
         binding = ActivityMain2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
 
-        final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_activity_main2);
-        final NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_devices, R.id.navigation_messages, R.id.navigation_downloads)
+                .build();
 
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main2);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-
-        navView.setOnItemSelectedListener(item -> {
-            if (item.getItemId() == R.id.navigation_home) {
-                item.setChecked(true);
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.nav_host_fragment_activity_main2, HomeFragment.class, null)
-                        .commit();
-            } else if (item.getItemId() == R.id.navigation_devices) {
-                // TODO: 8/15/2022 if logged in then go to devices 
-                item.setChecked(true);
-                if (true) {
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.nav_host_fragment_activity_main2, DevicesFragment.class, null)
-                            .commit();
-                } else {
-                    //Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                    //startActivity(intent);
-                    //navView.setSelectedItemId(R.id.navigation_downloads);
-                }
-
-
-            } else if (item.getItemId() == R.id.navigation_messages) {
-                item.setChecked(true);
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.nav_host_fragment_activity_main2, MessagesFragment.class, null)
-                        .commit();
-
-            } else if (item.getItemId() == R.id.navigation_downloads) {
-                item.setChecked(true);
-                getSupportFragmentManager()
-                        .beginTransaction()
+       /* navView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.navigation_devices) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                //navView.setSelectedItemId(R.id.navigation_downloads);
+            } else {
+                // Create new fragment and transaction
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
                         .replace(R.id.nav_host_fragment_activity_main2, DownloadsFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name")
                         .commit();
-
+                Log.d("tag", "onNavigationItemSelected: devices clicked");
             }
             return false;
-        });
+        });*/
 
     }
 
